ab920b099fc6cf206609587bdc70cdd951ff9bc1
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2018-04-05 21:33:00 +0200

    working with databases

--- tkinter/books.py
+++ tkinter/books.py
@@ -5,13 +5,33 @@ conn = sqlite3.connect("books.db")
 
 def create_table():
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS books (item TEXT, quantity INTEGER, price REAL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER, item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
 
 
-def insert(item, quantity, price):
+def update(id, item, quantity, price):
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO books VALUES(?, ?, ?)", (item, quantity, price))
+    cursor.execute("UPDATE books SET item = ?, quantity = ?, price = ? WHERE id = ?", (item, quantity, price, id))
+    conn.commit()
+
+
+def insert(id, item, quantity, price):
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM books WHERE id = ?", (id, ))
+
+    result = cursor.fetchone()
+
+    if result is not None:
+        return update(id, item, quantity, price)
+
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO books VALUES(?, ?, ?, ?)", (id, item, quantity, price))
+    conn.commit()
+
+
+def delete(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM books WHERE id = ?", (id, ))
     conn.commit()
 
 
@@ -23,8 +43,14 @@ def select():
 
 
 create_table()
-insert('DDD Book', 1, 20.99)
-insert('Clean code', 1, 21.99)
+insert(1, 'DDD Book', 1, 20.99)
+insert(2, 'Clean code', 1, 21.99)
+insert(3, 'Clean coder', 1, 25.69)
+
+print(select())
+
+delete(2)
+update(3, 'Clean coder - New Edition', 2, 26.89)
 
 print(select())
 
