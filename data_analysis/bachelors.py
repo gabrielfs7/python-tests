7531a924f8d216918c7914385b661d729935c6c8
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2018-04-15 11:35:41 +0200

    bokeh graph from bigger CSV

---
+++ data_analysis/bachelors.py
@@ -0,0 +1,21 @@
+import pandas
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+
+# Read info from CSV file and make a DataFrame
+data_frame = pandas.read_csv("bachelors.csv")
+
+# Set columns to figure based on the grath
+x = data_frame['Year']
+y = data_frame['Engineering']
+
+output_file('line_from_bachelors.html')
+
+# Create the figure object
+f = figure()
+
+# Draw graph
+f.line(x, y)
+
+# Show graph
+show(f)
