b13d4fa55e2c103b3a84159f187a0efd843a8623
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2018-04-22 18:09:23 +0200

    creating geocoding application

---
+++ geocoding-ws/README.md
@@ -0,0 +1,9 @@
+# Geocoding Web Service
+
+Python Geocoding webservice application
+
+
+1. User upload a CSV file.
+2. Application validate the CSV file and add columns "latitude" and "longitude" based on "address" column.
+3. Application display new content in a table.
+4. User Donwload new file with coordinates.
---
+++ geocoding-ws/application.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, send_file
+from werkzeug import secure_filename
+from geopy.geocoders import Nominatim
+import pandas
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    form_success = False
+    global file_name
+    global file_path
+    csv_lines = []
+
+    if request.method == 'POST':
+        file = request.files['file']
+        file_name = secure_filename(file.filename)
+        file_path = "downloads/processed-" + file_name
+        file.save(file_path)
+
+        df = pandas.read_csv(file_name)
+
+        nominatim = Nominatim(scheme="http")
+
+        df['Coordinates'] = df['Address']
+        df['Coordinates'] = df['Coordinates'].apply(nominatim.geocode)
+        df['Latitude'] = df['Coordinates'].apply(
+            lambda column_value: column_value.latitude if column_value != None else ''
+        )
+        df['Longitude'] = df['Coordinates'].apply(
+            lambda column_value: column_value.longitude if column_value != None else ''
+        )
+
+        # Remove temp column
+        df = df.drop('Coordinates', 1)
+
+        # Avoid save index to CSV
+        df.to_csv(file_path, index=False)
+
+        csv_lines = df.iterrows()
+
+        form_success = True
+
+    return render_template(
+        "index.html",
+        form_success=form_success,
+        csv_lines=csv_lines
+    )
+
+
+@app.route('/download')
+def download():
+    return send_file(
+        file_path,
+        attachment_filename=file_name,
+        as_attachment=True
+    )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
